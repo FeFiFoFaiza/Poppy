1cd14b234647a4da5d47a6aa78839a81b9f2bff5
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2021-09-27 20:15:44 -0400

    Update Time.java

--- pre/02/Time.java
+++ pre/02/Time.java
@@ -1,14 +1,20 @@
 public class Time {
     public static void main(String[] args) {
-        String day = "Tuesday";
-        int date = 17;
-        String month = "August";
-        int year = 2021;
-      
-        System.out.println("American format:");
-        System.out.println(day + ", " + month + " "+ date + ", " + year);
+        int hour = 15;
+        int minute = 21;
+        int second = 26;
+        int wholeSeconds = ((((60 * hour) + minute) * 60) + second);
 
-        System.out.println("European format:");
-        System.out.println(day + " " + date + " "+ month + " " + year);
+        System.out.println("Seconds since midnight: " + wholeSeconds);
+        System.out.println("seconds remaining: " + (86400 - wholeSeconds));
+        System.out.println("Percentage of day finished: " + (wholeSeconds * 100.0 / 86400.0));
+
+
+        hour = 15;
+        minute = 32;
+        second = 45;
+        int newWholeSeconds = ((((60 * hour) + minute) * 60) + second);
+
+        System.out.println("Time that has passed since doing this: " + (newWholeSeconds - wholeSeconds));
     }
 }
