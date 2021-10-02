f068628cf8f4d9bfa6916d2c80be2c6409457d72
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2021-10-02 15:48:55 -0400

    added BigSib

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,15 @@
+/*
+ TNPG: Tobias_Hillary_7 | Ajax, Diana Akhmedova, Tasnim Chowdhury, Faiza Huda, Mary, Truthful Tom
+ APCS
+ HW09 -- What are BigSibs Good for?
+ 2021-09-30
+
+*/
+
+public class BigSib{
+  
+  public static void Greet (String name) {
+    System.out.println("Oh look it's " + name + "! How do it do?");
+  }
+ 
+}
