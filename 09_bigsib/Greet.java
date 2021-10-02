2bd106a703fdc26d6b7fb0925de45c7fde4ccf25
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2021-10-02 15:44:37 -0400

    add to 09_bigsib

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,19 @@
+/*
+ TNPG: Tobias_Hillary_7 | Ajax, Diana Akhmedova, Tasnim Chowdhury, Faiza Huda, Mary, Truthful Tom
+ APCS
+ HW08 -- Refactor Freshie Zero
+ 2021-09-30
+ 
+ Discoveries:
+ - Diff classes diff files
+*/
+
+public class Greet{
+  
+  public static void main (String[]args) {
+    BigSib.greet("J.J. Binks");
+    BigSib.greet("Perchta");
+    BigSib.greet("Lil Big Duck");
+  }
+ 
+}
