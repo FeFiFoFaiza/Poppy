39678e3b5f898ced1ad5f1b61b785f0523335986
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2021-10-31 16:06:35 -0400

    Added description

--- cb/string-2/countCode.java
+++ cb/string-2/countCode.java
@@ -1,4 +1,10 @@
 public class countCode{
+	
+	/*
+	Return the number of times that the string "code" appears anywhere in the given string, 
+	except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+	*/
+	
 	public int countCode(String str) {
  	int codeCtr = 0;
   	for (int i = 2; i < str.length() - 1; i++){
