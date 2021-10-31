b512a56806fea3a8b3b26d89d40beab90c20275e
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2021-10-31 16:17:56 -0400

    Update countHi.java

--- cb/string-2/countHi.java
+++ cb/string-2/countHi.java
@@ -5,14 +5,14 @@ public class countHi{
 	*/
 	
 	public int countHi(String str) {
-  int hiCtr = 0;
-    for(int i = 0; i < str.length() - 1; i++){
-      if (str.substring(i, i + 2).equals("hi")){
-        hiCtr++;
-      }
-    }
-  return hiCtr;
-  }
+  		int hiCtr = 0;
+    		for(int i = 0; i < str.length() - 1; i++){
+      			if (str.substring(i, i + 2).equals("hi")){
+        			hiCtr++;
+      			}
+    		}
+  		return hiCtr;
+  	}
 
 	public static void main (String[] args){
 		countHi test = new countHi();
