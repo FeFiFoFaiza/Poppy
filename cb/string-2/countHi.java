8602f25754cd9265c7ade8f3019cbf2bf12afb28
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2021-10-31 16:09:24 -0400

    Create countHi.java

---
+++ cb/string-2/countHi.java
@@ -0,0 +1,24 @@
+public class countHi{
+	
+	/*
+	Return the number of times that the string "hi" appears anywhere in the given string.
+	*/
+	
+	public int countHi(String str) {
+  int hiCtr = 0;
+    for(int i = 0; i < str.length() - 1; i++){
+      if (str.substring(i, i + 2).equals("hi")){
+        hiCtr++;
+      }
+    }
+  return hiCtr;
+  }
+
+	public static void main (String[] args){
+		countHi test = new countHi();
+
+		System.out.println(test."hiho not HOHIhi")); // → 2
+		System.out.println(test.countCode("hiHIhi")); // → 2
+		System.out.println(test.countCode("h")); // → 0
+	}
+}
