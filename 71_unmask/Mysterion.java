93c5c4d84d8c05e4436d0427a5a3cd89a00146f6
Faiza Huda <58957309+FeFiFoFaiza@users.noreply.github.com>
2022-03-08 01:21:21 -0500

    Update Mysterion.java

--- 71_unmask/Mysterion.java
+++ 71_unmask/Mysterion.java
@@ -1,4 +1,28 @@
+import java.util.Arrays;
+
 public class Mysterion{
+  public static void mysterion(int[] arr, int a, int b, int c){
+    int v = arr[c];
+    swap(arr, c, b);
+    int s = a;
+    for(int i = a; i<b; i++){
+      if (arr[i] < v){
+        swap(arr, s, i);
+        s++;
+      }
+    }
+    swap(arr, b, s);
+  }
 
+  public static void swap (int[] arr, int a, int b){
+    int old = arr[a];
+    arr[a] = arr[b];
+    arr[b] = old;
+  }
 
+  public static void main(String[] args){
+    int[] handout = {7, 1, 5, 12, 3};
+    mysterion(handout, 0, 4, 2);
+    System.out.println(Arrays.toString(handout));
+  }
 }
